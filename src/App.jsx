87b64ed2975f457dd466a0ec147d19d9646fdb88
lyb5382 <lyb538202@gmail.com>
2025-08-14 16:21:33 +0900

    g

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import About from './pages/About'
+import Board from './pages/Board'
+import Home from './pages/Home'
+import Community from './pages/Community'
+import Notfound from './pages/Notfound'
+import BoardDetail from './pages/BoardDetail'
+import Footer from './components/Footer'
+function App() {
+
+  return (
+    <div>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/community' element={<Community/>}/>
+        <Route path='/board' element={<Board/>}/>
+        <Route path='/board/:id' element={<BoardDetail/>}/>
+        <Route path='*' element={<Notfound/>}/>
+      </Routes>
+    <Footer/>
+    </div>
+  )
+}
+
+export default App
